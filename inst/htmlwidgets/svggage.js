2c5a94e14d7a869a86787d79679379e56f30f69c
Sam Phinizy <sphinizy@mercycorps.org>
2019-02-12 21:10:44 -0500

    Moved the gauge creation to the render function.

--- inst/htmlwidgets/svggage.js
+++ inst/htmlwidgets/svggage.js
@@ -4,39 +4,38 @@ HTMLWidgets.widget({
 
   type: 'output',
 
-  factory: function(el, width, height) {
+  factory: function (el, width, height) {
 
-  var cpuGauge = Gauge(el, {
-    max: 100,
-    // custom label renderer
-    label: function(value) {
-      return Math.round(value) + "/" + this.max;
-    },
-    value: 50,
-    // Custom dial colors (Optional)
-    color: function(value) {
-      if(value < 20) {
-        return "#5ee432"; // green
-      }else if(value < 40) {
-        return "#fffa50"; // yellow
-      }else if(value < 60) {
-        return "#f7aa38"; // orange
-      }else {
-        return "#ef4655"; // red
-      }
-    }
-});
+
 
     return {
 
-      renderValue: function(x) {
-
-        // TODO: code to render the widget, e.g.
-       cpuGauge.setValue(x.value)
+      renderValue: function (x) {
+        
+        var gauge = Gauge(el, {
+          max: x.max,
+          // custom label renderer
+          label: function (value) {
+            return Math.round(value) + "/" + this.max;
+          },
+          value: x.value,
+          // Custom dial colors (Optional)
+          color: function (value) {
+            if (value < 20) {
+              return "#5ee432"; // green
+            } else if (value < 40) {
+              return "#fffa50"; // yellow
+            } else if (value < 60) {
+              return "#f7aa38"; // orange
+            } else {
+              return "#ef4655"; // red
+            }
+          }
+        });
 
       },
 
-      resize: function(width, height) {
+      resize: function (width, height) {
 
         // TODO: code to re-render the widget with a new size
 
